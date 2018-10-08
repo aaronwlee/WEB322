f61a31b271e20603825c6ca5b0a18a0767e69141
Lean Junio <leanjunio@live.com>
2018-10-08 10:23:12 -0400

    Update comment

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 /*********************************************************************************
-* WEB322: Assignment 2
+* WEB322: Assignment 03
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
 * No part of this assignment has been copied manually or electronically from any other source
 * (including web sites) or distributed to other students.
 *
-* Name: Lean Junio Student ID: 019-1091523 Date: September 24, 2018
+* Name: Lean Junio Student ID: 019-1091523 Date: October 08, 2018
 *
-* Online (Heroku) URL: https://sheltered-plateau-83740.herokuapp.com/
+* Online (Heroku) URL: https://intense-shore-93459.herokuapp.com/
 *
 ********************************************************************************/
 
