0be706ac2fa465bcec079eefea604e3b231ae6f5
Lean Junio <leanjunio@live.com>
2018-10-12 05:35:30 -0400

    changed port

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ app.set('views', path.join(__dirname, '/views'));
 app.use('/', routes);
 
 dataService.initialize()
-    .then(() => app.listen(PORT, () => console.log(`Listening on port ${PORT}`)))
+    .then(() => app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}`)))
     .catch(err => res.json({ message: err}))
 
