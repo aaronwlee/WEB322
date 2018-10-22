8fba3b9f8ef3fd0a4ce7eee6fc735ea38705c3b1
Lean Junio <ljjunio@myseneca.ca>
2018-10-22 16:30:43 -0400

    give us the ability to evaluate conditions for equality

--- server.js
+++ server.js
@@ -28,6 +28,15 @@ app.engine('.hbs', exphbs({
     helpers: {
         navLink: function(url, options) {
             return `<li` + ((url == app.locals.activeRoute) ? ' class="active" ' : '') + '><a href="' + url + '">' + options.fn(this) + '</a></li>'; 
+        },
+        equal: function(lvalue, rvalue, options) {
+            if (arguments.length < 3)
+                throw new Error("Handlebars helper equal needs 2 parameters");
+            if (lvalue != rvalue) {
+                return options.inverse(this);
+            } else {
+                return options.fn(this);
+            }
         }
     }
 }));
